f15b8ab3588f80699a49eee6e0e2c6b7951d483f
F1rrock <flyrock000@gmail.com>
2024-09-25 04:43:25 +0800

    code format has been refactored

--- lib/app_context.dart
+++ lib/app_context.dart
@@ -2,11 +2,16 @@ library app_context;
 
 export 'package:app_context/src/core/ctx/context.dart' show Context;
 export 'package:app_context/src/core/ctx/context_node.dart' show ContextNode;
-export 'package:app_context/src/core/errors/no_value_with_such_key.dart' show NoValueWithSuchKey;
-export 'package:app_context/src/core/errors/unresolved_value.dart' show UnresolvedValue;
-export 'package:app_context/src/core/storages/key_value_storage.dart' show KeyValueStorage;
+export 'package:app_context/src/core/errors/no_value_with_such_key.dart'
+    show NoValueWithSuchKey;
+export 'package:app_context/src/core/errors/unresolved_value.dart'
+    show UnresolvedValue;
+export 'package:app_context/src/core/storages/key_value_storage.dart'
+    show KeyValueStorage;
 export 'package:app_context/src/core/traits/trait.dart' show Trait;
-export 'package:app_context/src/utils/adapters/ctx_node_adapter.dart' show CtxNodeAdapter;
+export 'package:app_context/src/utils/adapters/ctx_node_adapter.dart'
+    show CtxNodeAdapter;
 export 'package:app_context/src/utils/ctx/std_context.dart' show StdContext;
-export 'package:app_context/src/common/in_memory_storage.dart' show InMemoryStorage;
+export 'package:app_context/src/common/in_memory_storage.dart'
+    show InMemoryStorage;
 export 'package:app_context/src/common/simple_trait.dart' show SimpleTrait;
